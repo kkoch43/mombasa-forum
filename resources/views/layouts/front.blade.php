2be06f95406640af1179f330ec123ef639638f64
Kevin Kipkkoech <Kevin Kipkkoech>
2017-07-27 01:07:50 +0300

    Designing the main/landing page

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoert"
+        content="width-device-width, user-scalable-no, initial-scale=1.0,maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-COMPATIBLE" content="ie-edge">
+    <title>Mombasa Forum</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+
+<body>
+<div class="navbar">
+    <a class="navbar-brand" href="#">Mombasa Forum</a>
+    <ul class="nav navbar-nav">
+        <li class="active">
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Link</a>
+        </li>
+    </ul>
+</div>
+
+<div class="container">
+    @yield('content')
+</div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"--}}
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+
+<!--latest compiled and minified JS -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
